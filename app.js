9dd22bed24676b5ec8dab1567ec1fdc9404c2e62
Ankit Shaji <ankitshaji@gmail.com>
2022-07-17 20:47:52 +1200

    create more element object and nest them

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 //Element object = Document Node/ElmentObject.method(css element selector)
+//parent element object - first occurence
 const container = document.querySelector("#container");
 const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 //element object creation-valueset-parent object append- loop
 for (let i = 1; i < 152; i++) {
-  //create img element object
+  //create div,span ,label,img element object - document object method(argument)
+  const pokeBox = document.createElement("div");
+  const label = document.createElement("span");
+  label.innerText = `#${i}`; //string template literal //human visible text
   const newImg = document.createElement("img");
-  //add value to src property of img element object
-  //string template literal
-  newImg.src = `${baseURL}${i}.png`;
-  //append inside parent element object(section #container) the child object element(img)
+  newImg.src = `${baseURL}${i}.png`; //string template literal //add value to src property of img element object
+
+  //append inside parent element object the child object element // console.dir(container)
   //adding to nodelist object-childNodes property of parent element object
   //adding to htmlcollection object - children property of parent element object
   //adding to lastChild of parent element
-  // console.dir(container)
-  container.appendChild(newImg);
+  pokeBox.appendChild(newImg);
+  pokeBox.appendChild(label); //could multi element object append if using append()
+  container.appendChild(pokeBox);
+}
+
+{
+  /* <div>
+  <img></img>
+  <span></span>
+</div> */
 }
